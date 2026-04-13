5add2af947e8a7cbda7a494c98b6bdf0402c461e
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-13 20:23:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,17 +20,6 @@ sed -i 's/192.168.\$((addr_offset++))/10.0.\$((addr_offset++))/g' package/base-f
 # 512布局
 sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000.dts
 
-# 切换防火墙版本
-sed -i 's/CONFIG_DEFAULT_firewall=y/# CONFIG_DEFAULT_firewall is not set/g' .config
-sed -i 's/CONFIG_DEFAULT_iptables=y/# CONFIG_DEFAULT_iptables is not set/g' .config
-sed -i 's/CONFIG_DEFAULT_ip6tables=y/# CONFIG_DEFAULT_ip6tables is not set/g' .config
-sed -i 's/CONFIG_DEFAULT_ipset=y/# CONFIG_DEFAULT_ipset is not set/g' .config
-# 强制开启 Firewall4 及其依赖
-echo "CONFIG_PACKAGE_firewall4=y" >> .config
-echo "CONFIG_PACKAGE_nftables-json=y" >> .config
-echo "CONFIG_PACKAGE_kmod-nft-core=y" >> .config
-echo "CONFIG_PACKAGE_kmod-nft-offload=y" >> .config
-
 # 删除软件
 rm -rf feeds/luci/applications/luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-vlmcsd
@@ -51,6 +40,18 @@ git clone https://github.com/sirpdboy/luci-app-wizard package/luci-app-wizard
 rm -rf feeds/luci/applications/luci-app-adguardhome
 git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardhome
 
+# 切换防火墙版本
+sed -i 's/CONFIG_DEFAULT_firewall=y/# CONFIG_DEFAULT_firewall is not set/g' .config
+sed -i 's/CONFIG_DEFAULT_iptables=y/# CONFIG_DEFAULT_iptables is not set/g' .config
+sed -i 's/CONFIG_DEFAULT_ip6tables=y/# CONFIG_DEFAULT_ip6tables is not set/g' .config
+sed -i 's/CONFIG_DEFAULT_ipset=y/# CONFIG_DEFAULT_ipset is not set/g' .config
+# 强制开启 Firewall4 及其依赖
+echo "CONFIG_PACKAGE_firewall4=y" >> .config
+echo "CONFIG_PACKAGE_nftables-json=y" >> .config
+echo "CONFIG_PACKAGE_kmod-nft-core=y" >> .config
+echo "CONFIG_PACKAGE_kmod-nft-offload=y" >> .config
+
+# 集成软件
 echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-wizard=y" >> .config
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> .config
