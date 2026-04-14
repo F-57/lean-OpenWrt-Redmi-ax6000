0de5d5c1dc65df81d98bde7c022fce7855dc21f0
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-15 02:41:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-
 # 1. 替换 LAN 口默认 IP (192.168.1.1 -> 10.0.0.1)
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
@@ -35,19 +34,20 @@ rm -rf package/lean/ddns-scripts_aliyun
 rm -rf package/lean/ddns-scripts_dnspod
 rm -rf feeds/luci/applications/luci-app-turboacc
 
-# 下载软件包
 rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-openlist2
-git clone https://github.com/sbwml/luci-app-airconnect package/luci-app-airconnect
-git clone https://github.com/sirpdboy/luci-app-lucky package/luci-app-lucky
-
-# Golang 与 MosDNS (特殊处理)
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata feeds/packages/net/mosdns feeds/luci/applications/luci-app-mosdns
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# 下载软件包
+git clone --depth=1 https://github.com/F-57/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/sbwml/luci-app-airconnect package/luci-app-airconnect
+git clone --depth=1 https://github.com/sirpdboy/luci-app-lucky package/luci-app-lucky
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 更改菜单名字 定义一个快捷函数：参数1是文件路径，参数2是原始文字，参数3是目标文字
 change_name() {
@@ -66,14 +66,13 @@ change_name() {
 change_name "package/mosdns/luci-app-mosdns/po/zh_Hans/mosdns.po" "MosDNS" "转发分流"
 change_name "feeds/luci/applications/luci-app-upnp/po/zh_Hans/upnp.po" "UPnP IGD & PCP" "端口转发"
 change_name "package/lucky/luci-app-lucky/po/zh_Hans/lucky.po" "Lucky" "大吉大利"
-change_name "package/openlist/luci-app-openlist2/po/zh_Hans/openlist2.po" "OpenList" "聚合网盘"
 change_name "feeds/luci/applications/luci-app-turboacc/po/zh_Hans/turboacc.po " "TurboACC" "网络加速"
 
 # 集成软件 预置编译选项 (写入 .config)
 cat >> .config <<EOF
+CONFIG_PACKAGE_luci-theme-aurora=y
 CONFIG_PACKAGE_luci-app-mosdns=y
 CONFIG_PACKAGE_luci-app-adguardhome=y
 CONFIG_PACKAGE_luci-app-lucky=y
 CONFIG_PACKAGE_luci-app-airconnect=y
 EOF
-
