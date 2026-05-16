448765534f6fa593b03eced63721ae8e5f777d1b
楠方 <80924478+F-57@users.noreply.github.com>
2026-05-17 02:42:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -97,14 +97,13 @@ fi
 # LED RGB灯效
 sed -i 's/\r$//' ./files/etc/config/my_led.lua
 chmod +x ./files/etc/config/my_led.lua
-# 联动开机自启（往 rc.local 里面塞入常驻后台命令）
 mkdir -p ./files/etc/
 if [ -f "package/base-files/files/etc/rc.local" ]; then
     cp package/base-files/files/etc/rc.local ./files/etc/rc.local
 else
     echo -e "#!/bin/sh -e\n\nexit 0" > ./files/etc/rc.local
 fi
-sed -i '/exit 0/i \nohup /etc/config/my_led.lua > /tmp/my_led.log 2>&1 &' ./files/etc/rc.local
+sed -i '/exit 0/i \/etc/config/my_led.lua > /tmp/my_led.log 2>&1 &' ./files/etc/rc.local
 chmod +x ./files/etc/rc.local
 
 # 集成软件 预置编译选项 (写入 .config)
